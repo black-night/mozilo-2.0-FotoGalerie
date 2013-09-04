d6153be586d92d269c9391606be5fb079d4a9fcd
black-night <black_night@gmx.net>
2013-09-04 20:57:02 +0200

    Aufruf für erstes, letztes, neustes und ältestes Foto hinzufügen.

--- FotoGalerie/index.php
+++ FotoGalerie/index.php
@@ -14,55 +14,26 @@ class FotoGalerie extends Plugin {
     * Gibt den HTML-Code zurueck, mit dem die Plugin-Variable ersetzt 
     * wird.
     * 
-    ***************************************************************/
-
-
-    function getContent($value) {
-        global $CMS_CONF;
+    ***************************************************************/	
+	const FG_LAST = "LetztesFoto";
+	const FG_FIRST = "ErstesFoto";
+	const FG_NEW = "NeustesFoto";
+	const FG_OLD = "ÄltestesFoto";
+	
+    function getContent($value) {       
         global $specialchars;
-        global $lang_gallery_cms;
-        $dir = PLUGIN_DIR_REL."FotoGalerie/";
-        $lang_gallery_cms = new Language($dir."sprachen/cms_language_".$CMS_CONF->get("cmslanguage").".txt");
         
-        $gal_request = $specialchars->replacespecialchars($specialchars->getHtmlEntityDecode($value),false);
-        
-        $GALERIE_DIR = BASE_DIR.GALLERIES_DIR_NAME."/".$gal_request."/";
-        $GALERIE_DIR_SRC = str_replace("%","%25",URL_BASE.GALLERIES_DIR_NAME."/".$gal_request."/");
-        
-        global $syntax;
-        $syntax->insert_jquery_in_head('jquery');
-        $syntax->insert_in_head($this->getHead());
-
-        $alldescriptions = false;
-        if(is_file($GALERIE_DIR."texte.conf.php"))
-        	$alldescriptions = new Properties($GALERIE_DIR."texte.conf.php");        
-
-        // Galerieverzeichnis einlesen
-        $picarray = getDirAsArray($GALERIE_DIR,"img");
-        $i = 0;
-        $result = "<div id=\"".$this->getIDName($gal_request)."\">";
-        for ($i=0; $i<count($picarray); $i++) {        
-          $result .=  "<a href=\"".$GALERIE_DIR_SRC.$specialchars->replaceSpecialChars($picarray[$i],true)."\" class=\"thumbnail-link\">"
-                      ."<img src=\"".$GALERIE_DIR_SRC.PREVIEW_DIR_NAME."/".$specialchars->replaceSpecialChars($picarray[$i],true)."\" "
-          		      ."alt=\"".$specialchars->rebuildSpecialChars($picarray[$i],true,true)."\" class=\"thumbnail\" "
-          		      ."data-glisse-big=\"".$GALERIE_DIR_SRC.$specialchars->replaceSpecialChars($picarray[$i],true)."\" "
-          		      ."title=\"".$this->getCurrentDescription($picarray[$i],$picarray,$alldescriptions)."\" "
-          		      ."rel=\"group1\" "
-          		      ." />"
-          		      ."</a>";                
+        $values = explode(",", $value);
+        if (count($values) == 1) {
+        	$gal_request = $specialchars->replacespecialchars($specialchars->getHtmlEntityDecode($values[0]),false);
+        	$result =  $this->getFullGalerie($gal_request);
+        }else if (count($values) == 2) {
+        	$gal_request = $specialchars->replacespecialchars($specialchars->getHtmlEntityDecode($values[1]),false);
+			$result = $this->getSpezialGalerie($values[0],$values[1]);
         }
-        $result .= "<script type=\"text/javascript\"> "
-        		   ."$(\"a\").click(function(e) { if ($(this).hasClass('thumbnail-link')) { e.preventDefault(); } });"
-        		   ."$(function () { $('.thumbnail').glisse({ "
-        		   ." changeSpeed: ".$this->getInteger($this->settings->get("changeSpeed"))
-        		   .", speed: ".$this->getInteger($this->settings->get("speed"))
-        		   .", effect:'".$this->getEffect()."'"
-        		   .", fullscreen: ".$this->getBooleanStr($this->settings->get("fullscreen")) 
-        		   .", copyright: '".$this->settings->get("copyright")
-                   ."' }); }); </script>";
-       	$result .= "<br /><br/ >".$this->settings->get("copyright");
-        $result .= "</div>";
+        
         return $result;
+
     } // function getContent
     
     
@@ -143,7 +114,12 @@ class FotoGalerie extends Plugin {
             // Download-URL
             array("http://software.black-night.org","Software by black-night"),
             # Platzhalter => Kurzbeschreibung
-            array('{FotoGalerie|}' => $lang_gallery_admin->get("config_fotogallery_plugin_name"))
+            array('{FotoGalerie|...}' => $lang_gallery_admin->get("config_fotogallery_plugin_name"),
+            	  '{FotoGalerie|'.self::FG_FIRST.',...}' => $lang_gallery_admin->get("config_fotogallery_plugin_first"),
+            	  '{FotoGalerie|'.self::FG_LAST.',...}' => $lang_gallery_admin->get("config_fotogallery_plugin_last"),
+            	  '{FotoGalerie|'.self::FG_NEW.',...}' => $lang_gallery_admin->get("config_fotogallery_plugin_new"),
+            	  '{FotoGalerie|'.self::FG_OLD.',...}' => $lang_gallery_admin->get("config_fotogallery_plugin_old")           		
+                 )
             );
             return $info;        
     } // function getInfo
@@ -153,6 +129,90 @@ class FotoGalerie extends Plugin {
     * Interne Funktionen
     *
     ***************************************************************/
+    function getFullGalerie($galleryname) {
+    	global $CMS_CONF;
+    	global $lang_gallery_cms;
+    	global $specialchars;
+    	$dir = PLUGIN_DIR_REL."FotoGalerie/";
+    	$lang_gallery_cms = new Language($dir."sprachen/cms_language_".$CMS_CONF->get("cmslanguage").".txt");    	
+    	$GALERIE_DIR = BASE_DIR.GALLERIES_DIR_NAME."/".$galleryname."/";
+    	$GALERIE_DIR_SRC = str_replace("%","%25",URL_BASE.GALLERIES_DIR_NAME."/".$galleryname."/");
+    	
+    	global $syntax;
+    	$syntax->insert_jquery_in_head('jquery');
+    	$syntax->insert_in_head($this->getHead());
+    	
+    	$alldescriptions = false;
+    	if(is_file($GALERIE_DIR."texte.conf.php"))
+    		$alldescriptions = new Properties($GALERIE_DIR."texte.conf.php");
+    	
+    	// Galerieverzeichnis einlesen
+    	$picarray = getDirAsArray($GALERIE_DIR,"img");
+    	$i = 0;
+    	$result = "<div id=\"".$this->getIDName($galleryname)."\">";
+    	for ($i=0; $i<count($picarray); $i++) {
+    		$result .=  "<a href=\"".$GALERIE_DIR_SRC.$specialchars->replaceSpecialChars($picarray[$i],true)."\" class=\"thumbnail-link\">"
+    				."<img src=\"".$GALERIE_DIR_SRC.PREVIEW_DIR_NAME."/".$specialchars->replaceSpecialChars($picarray[$i],true)."\" "
+    						."alt=\"".$specialchars->rebuildSpecialChars($picarray[$i],true,true)."\" class=\"thumbnail\" "
+    								."data-glisse-big=\"".$GALERIE_DIR_SRC.$specialchars->replaceSpecialChars($picarray[$i],true)."\" "
+    										."title=\"".$this->getCurrentDescription($picarray[$i],$picarray,$alldescriptions)."\" "
+    												."rel=\"group1\" "
+    														." />"
+    																."</a>";
+    	}
+    	$result .= "<script type=\"text/javascript\"> "
+    			."$(\"a\").click(function(e) { if ($(this).hasClass('thumbnail-link')) { e.preventDefault(); } });"
+    			."$(function () { $('.thumbnail').glisse({ "
+    					." changeSpeed: ".$this->getInteger($this->settings->get("changeSpeed"))
+    					.", speed: ".$this->getInteger($this->settings->get("speed"))
+    					.", effect:'".$this->getEffect()."'"
+    							.", fullscreen: ".$this->getBooleanStr($this->settings->get("fullscreen"))
+    							.", copyright: '".$this->settings->get("copyright")
+    							."' }); }); </script>";
+    	$result .= "<br /><br/ >".$this->settings->get("copyright");
+    	$result .= "</div>";
+    	return $result;    	
+    } //getFullGalerie
+    
+    function getSpezialGalerie($typ,$galleryname) {
+    	global $specialchars;
+    	$GALERIE_DIR = BASE_DIR.GALLERIES_DIR_NAME."/".$galleryname."/";
+    	$GALERIE_DIR_SRC = str_replace("%","%25",URL_BASE.GALLERIES_DIR_NAME."/".$galleryname."/");
+    	$picarray = getDirAsArray($GALERIE_DIR,"img");
+    	if ($typ == self::FG_LAST) {
+    		//Letztes Foto der Galerie laden
+    		$result = "<img src=\"".$GALERIE_DIR_SRC.PREVIEW_DIR_NAME."/".$specialchars->replaceSpecialChars($picarray[count($picarray)-1],true)."\" />";
+    	}elseif ($typ == self::FG_FIRST) {
+    		//Erste Foto der Galerie laden
+    		$result = "<img src=\"".$GALERIE_DIR_SRC.PREVIEW_DIR_NAME."/".$specialchars->replaceSpecialChars($picarray[0],true)."\" />";    		
+    	}elseif ($typ == self::FG_NEW) {
+    		//Neustes Foto der Galerie laden
+    		$chdate = 0;
+    		$index = 0;
+    		for ($i = 0; $i < count($picarray); $i++) {
+    			$tempchdate = filemtime($GALERIE_DIR."/".$picarray[$i]);
+    			if ($tempchdate>$chdate) {
+    				$chdate = $tempchdate;
+    				$index = $i;
+    			}
+    		}
+    		$result = "<img src=\"".$GALERIE_DIR_SRC.PREVIEW_DIR_NAME."/".$specialchars->replaceSpecialChars($picarray[$index],true)."\" />";
+    	}elseif ($typ == self::FG_OLD) {
+    		//Aeltestes Foto der Galerie laden
+    		$chdate = time();
+    		$index = 0;
+    		for ($i = 0; $i < count($picarray); $i++) {
+    			$tempchdate = filemtime($GALERIE_DIR."/".$picarray[$i]);
+    			if ($tempchdate<$chdate) {
+    				$chdate = $tempchdate;
+    				$index = $i;
+    			}
+    		}
+    		$result = "<img src=\"".$GALERIE_DIR_SRC.PREVIEW_DIR_NAME."/".$specialchars->replaceSpecialChars($picarray[$index],true)."\" />";    		
+    	}
+    	return $result;
+    } //getSpezialGalerie
+    
     function getHead() {   
     	$head = '<style type="text/css"> @import "'.URL_BASE.PLUGIN_DIR_NAME.'/FotoGalerie/css/glisse.css"; </style>'
     	        .'<script type="text/javascript" src="'.URL_BASE.PLUGIN_DIR_NAME.'/FotoGalerie/js/glisse.js"></script>'
